896d0c9b0ae5f279b5b3a1f0ad460ac08593b7d0
rmar72 <rsolm72@gmail.com>
2018-06-04 16:44:41 -0600

    campgrounds page to list campgrounds

--- app.js
+++ app.js
@@ -5,8 +5,18 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}))
 app.set('view engine', 'ejs');
 
+var campgrounds = [
+    {name: "Salmon Creek", image:"https://www.nps.gov/noca/planyourvisit/images/Thunder_Creek_Bridge_1.jpg?maxwidth=650&autorotate=false"},
+    {name: "Alpine Creek", image:"http://www.wandernorthgeorgia.com/wp-content/uploads/2016/09/dickscreek5.jpg"},
+    {name: "Bear Creek", image:"https://www.steamboatchamber.com/media/549263/Fish-Creek-Falls-Overlook.jpg"},
+];
+
 app.get('/', function(req, res){
     res.render('landing');
+});
+
+app.get('/campgrounds', function(req, res){
+    res.render("campgrounds", {campgrounds: campgrounds});
 });
 
 app.listen(3000, function(){
