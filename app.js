669fafb5e44c6e6992da2c2df79a2932d75217c7
rmar72 <rsolm72@gmail.com>
2018-06-06 17:21:49 -0600

    Show Route plus campground description

--- app.js
+++ app.js
@@ -9,37 +9,53 @@ app.set('view engine', 'ejs');
 // Schema Setup
 const campgroundSchema = new mongoose.Schema({
     name: String,
-    image: String
+    image: String,
+    description: String
 });
 
 const Campground = mongoose.model('Campground', campgroundSchema);
 
 app.get('/', (req, res) => res.render('landing'));
 
+// INDEX
 app.get('/campgrounds', (req, res) =>
-   
     Campground.find({}, (err, allCampgrounds) => {
         if(err)
             console.log(err);
         else
-            res.render("campgrounds", {campgrounds: allCampgrounds});
+            res.render("index", {campgrounds: allCampgrounds});
     })
 );
 
+// CREATE
 app.post('/campgrounds', (req, res) => {
-    let name = req.body.name;
-    let image = req.body.image;
-    let newCampground = { name: name, image: image }
+    let newCampground = { 
+        name: req.body.name,
+        image: req.body.image,
+        description: req.body.description
+    }
+
     Campground.create(newCampground, (err, newCampground) => {
         if(err)
             console.log(err);
         else
-            res.redirect('campgrounds');
+            res.redirect('/campgrounds');
     });
     
 });
 
+// NEW
 app.get('/campgrounds/new', (req, res) => res.render("new"));
+
+// SHOW
+app.get('/campgrounds/:id', (req, res) => {
+    Campground.findById(req.params.id, (err, campg) => {
+        if(err)
+            console.log(err);
+        else
+            res.render('show', {campground:campg});
+    });
+});
 
 app.listen(3000, () =>
     console.log("Yelp Camp Server up and running")
