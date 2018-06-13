a85bfaaaee1ecc9f2cf23a469ba1bac3a90f67b5
DESKTOP-Q66EQI4\rs7 <rsolm72@gmail.com>
2018-06-12 20:47:09 -0600

    passport auth configuration

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const   express = require('express'),
         app = express(),
         bodyParser = require('body-parser'),
         mongoose = require("mongoose"),
+        passport = require("passport"),
+        LocalStrategy = require("passport-local"),
         Campground = require('./models/campground'),
         Comment = require('./models/comment'),
+        User = require('./models/user'),
         seedDB = require('./seeds');
 
 mongoose.connect('mongodb://localhost/yelp_camp');
@@ -11,6 +14,20 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 //seedDB();
+
+//Passport Configuration
+app.use(require('express-session')({
+        secret:'Rusty Wins',
+        resave: false,
+        saveUninitialized: false
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 // Landing
 app.get('/', (req, res) => res.render('landing'));
