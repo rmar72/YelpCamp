db0cbfc9af0596ca91ac95d275f166faa14bb4ac
DESKTOP-Q66EQI4\rs7 <rsolm72@gmail.com>
2018-06-14 20:20:35 -0600

    Edit and Update campground. Made router methods and new page for edit

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const   express = require('express'),
         Campground = require('./models/campground'),
         Comment = require('./models/comment'),
         User = require('./models/user'),
+        methodOverride = require('method-override'),
         seedDB = require('./seeds');
 
 const   indexRoutes = require('./routes/index'),
@@ -17,6 +18,7 @@ mongoose.connect('mongodb://localhost/yelp_camp');
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
+app.use(methodOverride('_method'));
 //seedDB();
 
 //Passport Configuration
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -49,6 +49,27 @@ router.get('/:id', (req, res) => {
     });
 });
 
+// EDIT
+router.get('/:id/edit', (req, res) => {
+    Campground.findById(req.params.id, (err, campg)=>{
+        if(err)
+            console.log(err);
+        else {
+             res.render("campgrounds/edit", {campground: campg});
+        }
+    });
+   
+});
+
+// UPDATE
+router.put('/:id', (req, res) => {
+    Campground.findByIdAndUpdate(req.params.id, req.body.campground, (err, updatedCampg)=>{
+        if(err) console.log(err);
+        else{
+            res.redirect(`/campgrounds/${req.params.id}`);
+        }
+    });
+});
 function isLoggedIn(req, res, next){
     if(req.isAuthenticated()){
         return next();
