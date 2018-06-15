7fdbd996339f1ad320afe1c06effd7659d98ed48
DESKTOP-Q66EQI4\rs7 <rsolm72@gmail.com>
2018-06-14 21:46:46 -0600

    comments edit and update router methods

--- routes/comments.js
+++ routes/comments.js
@@ -38,6 +38,28 @@ router.post('/', isLoggedIn, (req, res) => {
     });
 });
 
+//EDIT
+router.get("/:comment_id/edit", (req, res)=>{
+    Comment.findById(req.params.comment_id, (err, foundComment)=>{
+        if(err)
+            res.redirect("back");
+        else
+            res.render('comments/edit', {campground_id: req.params.id, comment: foundComment});
+    });
+});
+
+// UPDATE
+router.put("/:comment_id", (req, res)=>{
+    Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, (err, updatedComment)=>{
+        if(err)
+            res.redirect("back");
+        else
+            res.redirect(`/campgrounds/${req.params.id}`);
+    });
+});
+
+// DELETE
+
 function isLoggedIn(req, res, next){
     if(req.isAuthenticated()){
         return next();
